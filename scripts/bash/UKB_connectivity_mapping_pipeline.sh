05f7d4cb880ddec5040efcaa55d7084cf64e6887
sina <sina.mansour.lakouraj@gmail.com>
2021-08-27 17:09:09 +1000

    finalized the atlas mapping automation scripts

--- scripts/bash/UKB_connectivity_mapping_pipeline.sh
+++ scripts/bash/UKB_connectivity_mapping_pipeline.sh
@@ -8,9 +8,13 @@
 # - Mapping functional time-series for the atlases
 # - Mapping structural connectivity for the alases
 # 
+# Usage:
+# 	UKB_connectivity_mapping_pipeline.sh <path_to_codes_directory> <path_to_subject_directory> <subject_id>
+# 
 # Notes:
-# - - the ${ukb_subjects_dir} must include a subdirectory for the subject id,
-# 	- the code uses the fsaverage files provided by freesurfer, so freesurfer 
+# - the ${ukb_subjects_dir} must include a subdirectory for the subject id,
+# - the paths provided should not contain spaces (FSL issue)
+# - the code uses the fsaverage files provided by freesurfer, so freesurfer 
 # 	  should be installed and $FREESURFER_HOME should be defined
 # 
 # Helpful resources:
@@ -79,6 +83,14 @@ atlases=(
 	# "Schaefer17n1000p,Schaefer2018_1000Parcels_17Networks_order.dlabel.nii"
 )
 
+subcortical_atlases=(
+	# Melbourne Subcortex Atlas
+	# "Tian_Subcortex_S1_3T,Tian_Subcortex_S1_3T.nii.gz"
+	# "Tian_Subcortex_S2_3T,Tian_Subcortex_S2_3T.nii.gz"
+	# "Tian_Subcortex_S3_3T,Tian_Subcortex_S3_3T.nii.gz"
+	"Tian_Subcortex_S4_3T,Tian_Subcortex_S4_3T.nii.gz"
+)
+
 # --------------------------------------------------------------------------------
 # Download subject data
 # --------------------------------------------------------------------------------
@@ -89,6 +101,8 @@ atlases=(
 # --------------------------------------------------------------------------------
 # Generate native volumetric atlases
 # --------------------------------------------------------------------------------
+
+# Step 1: map surface atlases to native surface
 
 echo -e "${GREEN}[INFO]`date`:${NC} Mapping surface atlases to native surface space."
 
@@ -110,6 +124,8 @@ done
 
 echo -e "${GREEN}[INFO]`date`:${NC} Completed mapping surface atlases to native surface space."
 
+# Step 2: map native surface atlases to native volumetric labels
+
 echo -e "${GREEN}[INFO]`date`:${NC} Mapping native surface atlases to native volume."
 
 # map all surface atlases to native volume
@@ -118,12 +134,59 @@ for atlas in ${atlases[@]}; do
 	atlas_name=${atlas_info[0]}
 	atlas_file=${atlas_info[1]}
 
+	echo -e "${GREEN}[INFO]`date`:${NC} Mapping ${atlas_name} to native volume."
+
 	# use the python code to map surface labels to native volume
-	python3 "${script_dir}/python/map_surface_label_to_volume.py" "${main_dir}" "${ukb_subjects_dir}" "${ukb_subject_id}" "${atlas_name}"
+	native_atlas_location="${temporary_dir}/subjects/${ukb_subject_id}/atlases/native.${atlas_name}.nii.gz"
+	if [ ! -f ${native_atlas_location} ]; then
+		python3 "${script_dir}/python/map_surface_label_to_volume.py" "${main_dir}" "${ukb_subjects_dir}" "${ukb_subject_id}" "${atlas_name}"
+	fi
+
+	# This native parcellation should now be resampled to the fmri space
+	native_atlas_location_fMRI="${temporary_dir}/subjects/${ukb_subject_id}/atlases/native.fMRI_space.${atlas_name}.nii.gz"
+	if [ ! -f ${native_atlas_location_fMRI} ]; then
+		mri_vol2vol --mov "${native_atlas_location}" --targ "${ukb_subjects_dir}/${ukb_subject_id}/fMRI/rfMRI.ica/mean_func.nii.gz" --interp nearest --regheader --o "${native_atlas_location_fMRI}"
+	fi
 done
 
+# Step 3: map subcortical labels from standard to native space
 
-# To be continued...
+echo -e "${GREEN}[INFO]`date`:${NC} Mapping MNI subcortical atlases to native volume."
+
+if [ ! -d "${temporary_dir}/subjects/${ukb_subject_id}/transforms" ]; then
+	mkdir -p "${temporary_dir}/subjects/${ukb_subject_id}/transforms"
+fi
+
+# compute inverse warp required for warping labels to native
+echo -e "${GREEN}[INFO]`date`:${NC} Computing the inverse warp."
+inverse_warp="${temporary_dir}/subjects/${ukb_subject_id}/transforms/MNI_to_T1_inversed_warp_coef.nii.gz"
+if [ ! -f ${inverse_warp} ]; then
+	invwarp --ref="${ukb_subjects_dir}/${ukb_subject_id}/T1/T1_brain.nii.gz" --warp="${ukb_subjects_dir}/${ukb_subject_id}/T1/transforms/T1_to_MNI_warp_coef.nii.gz" --out="${inverse_warp}"
+fi
+
+# map all subcortical atlases to native volume
+# mainly, we'll use the inverse warp to transform the subcortical atlas to native space
+for atlas in ${subcortical_atlases[@]}; do
+	IFS=',' read -a atlas_info <<< "${atlas}"
+	atlas_name=${atlas_info[0]}
+	atlas_file=${atlas_info[1]}
+
+	echo -e "${GREEN}[INFO]`date`:${NC} Mapping ${atlas_name} to native volume."
+
+	atlas_location="${template_dir}/atlases/${atlas_file}"
+
+	# use FSL's applywarp to map subcortical labels to native volume
+	native_atlas_location="${temporary_dir}/subjects/${ukb_subject_id}/atlases/native.${atlas_name}.nii.gz"
+	if [ ! -f ${native_atlas_location} ]; then
+		applywarp --ref="${ukb_subjects_dir}/${ukb_subject_id}/T1/T1_brain.nii.gz" --in="${atlas_location}" --warp="${inverse_warp}" --out="${native_atlas_location}" --interp=nn
+	fi
+
+	# This native parcellation should now be resampled to the fmri space
+	native_atlas_location_fMRI="${temporary_dir}/subjects/${ukb_subject_id}/atlases/native.fMRI_space.${atlas_name}.nii.gz"
+	if [ ! -f ${native_atlas_location_fMRI} ]; then
+		mri_vol2vol --mov "${native_atlas_location}" --targ "${ukb_subjects_dir}/${ukb_subject_id}/fMRI/rfMRI.ica/mean_func.nii.gz" --interp nearest --regheader --o "${native_atlas_location_fMRI}"
+	fi
+done
 
 
 # --------------------------------------------------------------------------------
