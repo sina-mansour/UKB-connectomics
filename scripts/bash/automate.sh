d6f1c344fe7af96f49b04c2f62dbe51e1e663585
sina <sina.mansour.lakouraj@gmail.com>
2021-10-01 16:18:13 +1000

    added slurm scripts used to automate the mapping pipeline on Spartan hpc

---
+++ scripts/bash/automate.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH --account punim1566
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0-05:00:00
+#SBATCH --mem=16G
+#SBATCH --partition=physical
+#SBATCH -o /data/gpfs/projects/punim1566/UKB-slurm/slurm_logs/%x_%j.out
+
+# This script is used for automated connectivity mapping for UKB
+#
+# Usage: ./automate.sh <subject-idx>
+#
+
+source /usr/local/module/spartan_new.sh
+module load foss/2019b
+module load web_proxy/latest connectomeworkbench/1.4.2 freesurfer/7.1.1-centos7_x86_64 fsl/6.0.3-python-3.7.4
+
+source ukbvenv/bin/activate
+
+# make sure that the virtual environment is in pythonpath
+[[ ":$PYTHONPATH:" != *":/data/gpfs/projects/punim1566/UKB-slurm/ukbvenv/lib/python3.7/site-packages:"* ]] && PYTHONPATH="/data/gpfs/projects/punim1566/UKB-slurm/ukbvenv/lib/python3.7/site-packages:${PYTHONPATH}"
+
+# some colors for fancy logging :D
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+# read the arguments
+index=$1
+
+# change directory to scripts directory
+code_dir="/data/gpfs/projects/punim1566/UKB-connectomics"
+# data_dir="/data/gpfs/projects/punim1566/UKB-download"
+# instead, download to scratch space
+data_dir="/data/gpfs/projects/punim1566/UKB-download"
+
+cd ${code_dir}
+
+# run the automation command
+"${code_dir}/scripts/bash/UKB_connectivity_mapping_pipeline.sh" "${code_dir}" "${data_dir}" "${index}"
+
+echo -e "${GREEN}[INFO]${NC} `date`: Script finished!"
+
+deactivate
+
---
+++ scripts/bash/run_automization.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# this code is supposed to run in a high performance computing cluster login node with
+# network access to initiate slurm jobs for execution of automated ukbiobank connectivity
+# mapping.
+#
+# Usage: run_automization.sh ...
+#
+
+module load Python/3.6.4-intel-2017.u2
+source venv/bin/activate
+
+# some colors for fancy logging :D
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}[INFO]${NC} `date`: Starting..."
+
+start_index=$1
+end_index=$(($2 + 1))
+
+index=$start_index
+while [ $index -lt $end_index ]
+do
+        echo -e "${GREEN}[INFO]${NC} `date`: Submitting job for: instance ${index}"
+        sbatch --job-name="ukb_${index}" --comment="UKB mapping pipeline, instance #${subject}" automate.sh ${index}
+        sleep 0.1
+        ((index = index + 1))
+done
+
+echo -e "${GREEN}[INFO]${NC} `date`: All jobs submitted."
+
+deactivate
+
