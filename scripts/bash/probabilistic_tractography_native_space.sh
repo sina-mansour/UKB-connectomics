ebee9ae3b9e9d8a4aad0165f8f9e6ab75dfb3b4c
sina <sina.mansour.lakouraj@gmail.com>
2022-04-25 13:53:33 +1000

    removed inline feedback comments to clean up the script (discussed in #21)

--- scripts/bash/probabilistic_tractography_native_space.sh
+++ scripts/bash/probabilistic_tractography_native_space.sh
@@ -179,21 +179,8 @@ fi
 # - maxlength is set to 250mm as the default option results in streamlines
 #   being at most 100 * voxelsize which will be 200mm and may result in loss
 #   of long streamlines for UKB data resolution
-# - not using multiple threads to speed-up (as it might cause lesser jobs accepted in the queue)
-#############################################################################################
-# RS: 
-# - In addition to testing execution speed between iFOD1 and iFOD2, you could also try iFOD2 with
-#   -samples 3 (and possibly also then with -power 0.5 as per point above), and see the extent to
-#   which that improves execution speed.
-# - Theoretically, you could do a combination of GM-WM interface seeding and homogeneous WM seeding.
-#   You could then have connectomes with half the streamline count using data from each individually,
-#   and connectomes using the concatenation of the two tractograms, which would potentially somewhat
-#   mitigate the biases present in each. FBC quantification would use the concatenation of the two.
-#   But this may be exposing a feature of the reconstruction pipeline that we explicitly *don't want*
-#   to explore?
-# - Still discussion to be had RE: requesting specific number of seeds vs. streamlines vs. selected streamlines
-#   https://github.com/MRtrix3/mrtrix3/issues/2391
-#############################################################################################
+# - not using multiple threads to speed-up (as it might cause fewer jobs being
+#   accepted in the queue)
 # testing with a smaller value: for 100K seeds, it took ~70sec, see below:
 # tckgen: [100%]   100000 seeds,    44305 streamlines,    17317 selected
 tracks="${dmri_dir}/tracks_${streamlines}.tck"
@@ -235,27 +222,6 @@ if [ ! -f ${sift_weights_npy} ]; then
     ${mrtrix_dir}/tcksift2 ${threading} -info "${tracks}" -act "${freesurfer_5tt}" -config NPYFloatMaxSavePrecision 16 \
                            -csv "${sift_stats}" "${wm_fod_norm}" "${sift_weights_npy}"
 fi
-
-# diffusion tensor metrics computed from provided images
-# dti_mif="${dmri_dir}/dti.mif"
-# if [ ! -f ${dti_mif} ]; then
-#     echo -e "${GREEN}[INFO]${NC} `date`: Diffusion tensor estimation"
-#     dwi2tensor ${threading} -info "${dwi_mif}" "${dti_mif}" -mask "${dwi_meanbzero_brain_mask}"
-# fi
-
-# # diffusion tensor metric computation (~1sec)
-# fa_mif="${dmri_dir}/dti_metric_fa.mif"
-# ad_mif="${dmri_dir}/dti_metric_ad.mif"
-# rd_mif="${dmri_dir}/dti_metric_rd.mif"
-# md_mif="${dmri_dir}/dti_metric_md.mif" # aka adc
-# cl_mif="${dmri_dir}/dti_metric_cl.mif"
-# cp_mif="${dmri_dir}/dti_metric_cp.mif"
-# cs_mif="${dmri_dir}/dti_metric_cs.mif"
-# if [ ! -f ${fa_mif} ]; then
-#     echo -e "${GREEN}[INFO]${NC} `date`: Computing tensor metrics (FA, MD, RD, AD, CL, CP, CS)"
-#     tensor2metric ${threading} -info "${dti_mif}" -fa "${fa_mif}" -ad "${ad_mif}" -rd "${rd_mif}" \
-#                   -adc "${md_mif}" -cl "${cl_mif}" -cp "${cp_mif}" -cs "${cs_mif}"
-# fi
 
 # sample metrics along streamlines (~4min) -> can be reduced by not using precise
 streamline_length_npy="${temporary_dir}/subjects/${ukb_subject_id}_${ukb_instance}/tractography/metrics/streamline_metric_length.npy"
@@ -302,57 +268,4 @@ if [ ! -f ${endpoints_npy} ]; then
     python3 "${script_dir}/python/save_endpoints_as_npy.py" "${endpoints}" "${endpoints_npy}"
 fi
 
-# Tractography considerations:
-# IFOD1 vs. IFOD2
-# Mean FA, MD, tensor eigenvecs, length, mean 5tt, diffusion kurtosis fit, freewater,
-
-
-###############################################################################################
-# RS:
-# - We had discussed the prospect of modifying the tcksample command to derive, for each
-#   streamline trajectory, samples from multiple quantitative 3D images, so that tcksample would
-#   not need to be executed once per quantitative measure, which could be slow due simply to 
-#   having to read through the track file once per quantitative measure.
-# - One I did not think of at the time is that we could theoretically do the same approach as
-#   above, but for connectome construction. tck2connectome could receive as input a 4D image, where
-#   each volume of the series is a unique hard parcellation, and the command would generate in
-#   a single invocation one connectome per volume. This would however require a decent amount of
-#   work on my part, so we'd need to see how many parcellations you want to use and how long
-#   tck2connectome takes per execution to know whether or not this is worthwhile.
-# - As per a prior point above: if the tractogram were stored on a RAM filesystem rather than
-#   on a network file share, then the time spent loading streamlines data in both tcksample and
-#   tck2connectome would be much shorter.
-# - Potential additional connectivity metrics:
-#   - Properties of multi-tissue decomposition (e.g. isometric log-ratios)
-#   - Pre-calculated NODDI parameters
-#   - FOD amplitude (existing code can only do exact FOD amplitude at streamline tangent;
-#     code for sampling AFD as the integral of the FOD lobe is part of a very large changeset that
-#     I started ~ 8 years ago; if it's considered highly desirable I can try to get it into a
-#     working state)
-#   - Note that pre-calculated tensors in UKB (presumably via FSL) will differ to those
-#     calculated by MRtrix3's dwi2tensor. Ours uses a thoroughly-evaluated iterative reweighting
-#     strategy (see references in command help page). So worth contemplating whether to use the
-#     existing pre-calculated tensors, or re-calculate using MRtrix3.
-# - Note also that for each quantitative metric, taking the *mean* value along the streamline
-#   trajectory and attributing that single scalar value to the streamline is the typical solution,
-#   but it's not a unique solution; one can also take e.g. the minimum value along the streamline
-#   trajectory (see tcksample -stat_tck option). Many combinations of voxel-wise quantitative metric
-#   and streamline-wise statistic will not make sense; but it's nevertheless worth considering the
-#   whole space of possibilities.
-###############################################################################################
-
 echo -e "${GREEN}[INFO]${NC} `date`: Finished tractography for: ${ukb_subject_id}_${ukb_instance}"
-
-
-################################################################################################
-# RS: As per discussion, need to find out how much data can be uploaded per subject to UKB
-# (and indeed what volume of data could potentially be hosted elsewhere). Any temporaries that
-# are not to be later hosted anywhere are better off being stored on a RAM file system.
-# My typical approach here is to load all input data into a scratch directory that I can force
-# to be in /tmp/, store all intermediate files and final outputs there, and only upon script
-# completion do I then write the desired derivatives to the location requested by the user.
-# I then only retain the scratch directory if the user explicitly requests that it be retained.
-# Your structure here checks for the pre-existence of calculated files, which is useful when you
-# are testing perturbations to the script, but for final deployment this ability is not as high
-# a priority.
-################################################################################################
