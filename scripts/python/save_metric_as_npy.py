efadff0d85c9cde2d4449b88aab34dd577223988
sina <sina.mansour.lakouraj@gmail.com>
2022-04-26 15:57:40 +1000

    :zap: removed some redundent code from a python script

--- scripts/python/save_metric_as_npy.py
+++ scripts/python/save_metric_as_npy.py
@@ -2,23 +2,12 @@
 
 import os
 import sys
-import time
-import datetime
 import numpy as np
 
 
 def ensure_dir(file_name):
     os.makedirs(os.path.dirname(file_name), exist_ok=True)
     return file_name
-
-
-def time_str(mode='abs', base=None):
-    if mode == 'rel':
-        return str(datetime.timedelta(seconds=(time.time() - base)))
-    if mode == 'raw':
-        return time.time()
-    if mode == 'abs':
-        return time.asctime(time.localtime(time.time()))
 
 
 if __name__ == '__main__':
